cbaa556563feebae925bd62a8f5ade55c464a7a0
Danilo Bargen <mail@dbrgn.ch>
2020-06-06 23:19:51 +0200

    Firmware: Reorganize task delays

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,3 +1,29 @@
+//! Sensilo Node Firmware
+//!
+//! ## Tasks
+//!
+//! The following tasks make up a measurement cycle:
+//!
+//! +----+    +-----------------+        +-------------------+
+//! |init| -> |start_measurement| -(t)-> |collect_measurement|
+//! +----+    +-----------------+        +-------------------+
+//!                                                   |
+//!                                                   v
+//!                                     +----------------+
+//!                                 +-- |broadcast_beacon|
+//!                                 |   +----------------+
+//!                                 |        ^    |
+//!                                 |        |    |
+//!                                 +--(b)---+    |
+//!                                               v
+//!                                       +----------+
+//!                                       |power_down|
+//!                                       +----------+
+//! Delays:
+//!
+//! - `t`: Max measurement duration for the sensor used
+//! - `b`: BEACON_BURST_INTERVAL_MS
+
 #![no_main]
 #![cfg_attr(not(test), no_std)]
 
@@ -16,16 +42,25 @@ use rubble_nrf5x::{
     radio::{BleRadio, PacketBuffer},
     utils::get_device_address,
 };
-use shtcx::{shtc1, Measurement, ShtC1};
+use shtcx::{shtc1, ShtC1};
 
 mod monotonic_nrf52;
 
-use monotonic_nrf52::U32Ext;
+use monotonic_nrf52::{Instant, U32Ext};
 
-const MEASURE_INTERVAL_MS: u32 = 210; // Should be divisible by 3
+// Measure at a specific interval
+const MEASURE_INTERVAL_MS: u32 = 3000;
 
+// Send 3 beacons, spaced 20 ms apart
+const BEACON_BURST_COUNT: u8 = 3;
+const BEACON_BURST_INTERVAL_MS: u32 = 20;
+
+// Sensor types
 const SENSOR_TEMP: u8 = 0x01;
 const SENSOR_HUMI: u8 = 0x02;
+
+// BLE Beacon
+const AD_STRUCTURE_MANUFACTURER_DATA: u8 = 0xff;
 
 #[app(device = crate::pac, peripherals = true, monotonic = crate::monotonic_nrf52::Tim1)]
 const APP: () = {
@@ -40,6 +75,12 @@ const APP: () = {
 
         // Measurements
         sht: ShtC1<hal::twim::Twim<pac::TWIM0>>,
+        #[init(None)]
+        measurement_start: Option<Instant>,
+
+        // Beacon
+        #[init(None)]
+        beacon: Option<Beacon>,
     }
 
     #[init(resources = [ble_tx_buf, ble_rx_buf], spawn = [start_measurement])]
@@ -110,22 +151,43 @@ const APP: () = {
     }
 
     /// Start a measurement
-    #[task(resources = [sht], schedule = [collect_measurement])]
+    #[task(resources = [sht, measurement_start], schedule = [collect_measurement])]
     fn start_measurement(ctx: start_measurement::Context) {
-        ctx.resources
-            .sht
-            .start_measurement(shtcx::PowerMode::NormalMode)
-            .unwrap();
+        let sht = ctx.resources.sht;
+        let power_mode = shtcx::PowerMode::NormalMode;
+
+        // Store the instant when this task was scheduled.
+        // This ensures that there is no jitter in scheduling.
+        *ctx.resources.measurement_start = Some(ctx.scheduled);
+
+        // Trigger measurement
+        sht.start_measurement(power_mode).unwrap();
 
         // Schedule measurement collection
+        let timedelta = (shtcx::max_measurement_duration(sht, power_mode) as u32).micros();
         ctx.schedule
-            .collect_measurement(ctx.scheduled + (MEASURE_INTERVAL_MS / 3).millis())
+            .collect_measurement(Instant::now() + timedelta)
             .unwrap();
     }
 
-    /// Collect a measurement
-    #[task(resources = [sht], schedule = [broadcast_beacon])]
+    /// Collect a measurement. Then send the data using non-connectable BLE
+    /// advertisement frames (beacons).
+    #[task(
+        resources = [sht, measurement_start, device_address, beacon],
+        schedule = [start_measurement],
+        spawn = [broadcast_beacon],
+    )]
     fn collect_measurement(ctx: collect_measurement::Context) {
+        static mut COUNTER: u16 = 0;
+
+        // Take measurement start time
+        let measurement_start = ctx
+            .resources
+            .measurement_start
+            .take()
+            .expect("Cannot collect measurement without starting a measurement first");
+
+        // Collect measurement result from sensor
         let measurement = ctx.resources.sht.get_measurement_result().unwrap();
         rprintln!(
             "SHTC1 measurement: {}°C / {} %RH",
@@ -133,52 +195,69 @@ const APP: () = {
             measurement.humidity.as_percent()
         );
 
-        // Schedule beacon transmission
-        ctx.schedule
-            .broadcast_beacon(
-                ctx.scheduled + (MEASURE_INTERVAL_MS / 3).millis(),
-                measurement,
-            )
-            .unwrap();
-    }
-
-    /// Broadcast the beacon exactly once.
-    #[task(resources = [radio, device_address], schedule = [start_measurement])]
-    fn broadcast_beacon(ctx: broadcast_beacon::Context, measurement: Measurement) {
-        // Beacon payload
+        // Prepare beacon payload
         let temp = measurement
             .temperature
             .as_millidegrees_celsius()
             .to_le_bytes();
         let humi = measurement.humidity.as_millipercent().to_le_bytes();
+        let counter_bytes = COUNTER.to_le_bytes();
+        #[rustfmt::skip]
+        let payload = [
+            0xff, 0xff,
+            counter_bytes[0], counter_bytes[1],
+            SENSOR_TEMP, temp[0], temp[1], temp[2], temp[3], // i32 LE
+            SENSOR_HUMI, humi[0], humi[1], humi[2], humi[3], // i32 LE
+        ];
 
         // Create beacon
-        let TYPE_MANUFACTURER_DATA = 0xff;
-        let beacon = Beacon::new(
-            *ctx.resources.device_address,
-            &[
-                AdStructure::CompleteLocalName("Sensilo"),
-                AdStructure::Unknown {
-                    ty: TYPE_MANUFACTURER_DATA,
-                    data: &[0xff, 0xff, SENSOR_TEMP, temp[0], temp[1], temp[2], temp[3]], // i32 LE
-                },
-                AdStructure::Unknown {
-                    ty: TYPE_MANUFACTURER_DATA,
-                    data: &[0xff, 0xff, SENSOR_HUMI, humi[0], humi[1], humi[2], humi[3]], // i32 LE
-                },
-            ],
-        )
-        .expect("Could not create beacon");
+        let advertisement_data = [
+            AdStructure::CompleteLocalName("Sensilo"),
+            AdStructure::Unknown {
+                ty: AD_STRUCTURE_MANUFACTURER_DATA,
+                data: &payload,
+            },
+        ];
+        let beacon = Beacon::new(*ctx.resources.device_address, &advertisement_data)
+            .expect("Could not create beacon");
+        *ctx.resources.beacon = Some(beacon);
+        rprintln!("Created beacon with counter {}", COUNTER);
 
         // Broadcast beacon
-        beacon.broadcast(ctx.resources.radio);
+        if ctx.spawn.broadcast_beacon(0).is_err() {
+            rprintln!("Error: Could not spawn broadcast_beacon");
+        }
+
+        // Increment counter
+        *COUNTER += 1;
 
         // Schedule a new measurement
         ctx.schedule
-            .start_measurement(ctx.scheduled + (MEASURE_INTERVAL_MS / 3).millis())
+            .start_measurement(measurement_start + MEASURE_INTERVAL_MS.millis())
             .unwrap();
+    }
 
-        rprintln!("Sent beacon");
+    /// Broadcast the beacon until the BEACON_BURST_COUNT has been reached.
+    #[task(resources = [radio, beacon], schedule = [broadcast_beacon])]
+    fn broadcast_beacon(ctx: broadcast_beacon::Context, i: u8) {
+        if i >= BEACON_BURST_COUNT {
+            return;
+        }
+
+        if let Some(beacon) = ctx.resources.beacon {
+            beacon.broadcast(ctx.resources.radio);
+            rprintln!("Sent beacon");
+
+            if ctx
+                .schedule
+                .broadcast_beacon(ctx.scheduled + BEACON_BURST_INTERVAL_MS.millis(), i + 1)
+                .is_err()
+            {
+                rprintln!("Error: Could not re-schedule broadcast_beacon");
+            }
+        } else {
+            rprintln!("Error: No beacon that can be broadcasted");
+        }
     }
 
     // Provide unused interrupts to RTFM for its scheduling
